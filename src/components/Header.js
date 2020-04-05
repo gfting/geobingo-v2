df36014cd6decf22b7124f87a634aae47c382f1b
Tim O'Brien <timothy.c.obrien99@gmail.com>
2020-04-05 11:59:15 -0400

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,49 +2,23 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { withRouter } from 'react-router'
 import { AUTH_TOKEN } from '../constants'
-
 /* eslint-disable */
-
 class Header extends Component {
-  render() {
+render() {
     const authToken = localStorage.getItem(AUTH_TOKEN)
     return (
-      <div className="flex pa1 justify-between nowrap orange">
+<div className="flex pa1 justify-between nowrap plum">
+        <div className="flex flex-fixed black">Make your very own custom bingo</div>
         <div className="flex flex-fixed black">
-          <div className="fw7 mr1">GeoBingo!</div>
-          <div>Make your very own custom bingo</div>
-          <Link to="/test" className="ml1 no-underline justify-right blue">
-            create new
-          </Link>
-          {authToken && (
-            <div className="flex">
-              <div className="ml1">|</div>
-              <Link to="/create" className="ml1 no-underline black">
-                submit
-              </Link>
-            </div>
-          )}
-        </div>
+          <div className="fw7 mr1">GeoBingo!</div>      
+</div>
         <div className="flex flex-fixed">
-          {authToken ? (
-            <div
-              className="ml1 pointer black"
-              onClick={() => {
-                localStorage.removeItem(AUTH_TOKEN)
-                this.props.history.push(`/`)
-              }}
-            >
-              logout
-            </div>
-          ) : (
-              <Link to="/login" className="ml1 no-underline black">
-                login
-            </Link>
-            )}
+        <Link to="/test" className="ml1 no-underline justify-right darkblue">
+        Create New Bingo
+      </Link>
         </div>
       </div>
     )
   }
 }
-
 export default withRouter(Header)
